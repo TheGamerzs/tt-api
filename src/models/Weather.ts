d301ecda6b5db0908d9628a6ffd320673b4c3266
logan <4rz4odjac@relay.firefox.com>
2021-04-08 21:30:56 +0100

    update weather to accurate names

--- src/models/Weather.ts
+++ src/models/Weather.ts
@@ -2,5 +2,5 @@ export interface Weather {
   hour: number;
   minute: number;
   time_remaining: number;
-  current_weather: 'extra sunny' | 'clear' | 'neutral' | 'smoggy' | 'foggy' | 'overcast' | 'cloudy' | 'drizzling' | 'rainy' | 'stormy' | 'snowy' | 'blizzardy' | 'drizzling snow' | 'snowy' | 'spooky';
+  current_weather: 'extra sunny' | 'clear' | 'neutral' | 'smoggy' | 'foggy' | 'overcast' | 'cloudy' | 'drizzling' | 'rainy' | 'stormy' | 'snowy' | 'blizzardy' | 'drizzling snow' | 'cold' | 'spooky';
 }
